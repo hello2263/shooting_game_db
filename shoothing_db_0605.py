811d7ec2757b3efb062831a9108b8026b5b4438a
Dogyu <hello2263@naver.com>
2021-06-05 16:50:59 +0900

    db의 연동과 삽입, 출력
    amazon RDS로 mysql DB를 만든 다음 파이썬으로 DB와 접속 후 shooting_db안에 Level1 테이블은 만들고 name,score를 삽입하거나 출력할 수 있도록 만들었으며 확인은 HeidiSQL프로그램을 통해 DB안에 데이터 확인

---
+++ shoothing_db_0605.py
@@ -0,0 +1,30 @@
+import pymysql
+
+# db연결
+db = pymysql.connect(host="shooting-db.cgb6v3f3cnyt.us-east-2.rds.amazonaws.com",
+                     user="dogyu", password="dogyu0081", charset="utf8")
+
+cursor = db.cursor(pymysql.cursors.DictCursor)
+
+cursor.execute('USE shooting_db;')
+
+# 삽입
+# cursor.execute('INSERT INTO Level1(name, score) VALUES ("황도규", 700)')
+
+# 출력
+cursor.execute('SELECT * FROM Level1;')
+value = cursor.fetchall()
+print(value[0])
+
+
+
+
+
+#갱신
+# cursor.execut('UPDATE lang SET descriptiom = "ZHTML is for the form web" WHERE name = "HTML"')
+
+#삭제
+# cursor.execute('DELETE FROM lang WHERE name="Java"')
+
+db.commit()
+db.close()
